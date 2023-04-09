aa341c08cc73e2389c20858aca2d74a74c6fe30c
Ziqi He <52439236+iheziqi@users.noreply.github.com>
2023-04-09 22:51:38 +0200

    refactored menu scraper

--- database/src/main.ts
+++ database/src/main.ts
@@ -1,5 +1,5 @@
 import { MensaMenu, mensaList } from './mensa';
-import { getMensaMenu, hasMenu } from './getMensaMenu';
+import { MenuScraper } from './getMensaMenu';
 import { getCurrentDate } from './utils/utils';
 import { MenuDB, UserDB, SubscriptionDB } from './database';
 import { cwd } from 'process';
@@ -12,7 +12,3 @@ enum MensaID {
 	mohm = 'mohm',
 	ishc = 'isch',
 }
-
-const menuDB = new MenuDB(dbPath);
-const userDB = new UserDB(dbPath);
-const subscriptionDB = new SubscriptionDB(dbPath);
