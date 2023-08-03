35f58c0408c728d6a5d12a9ee1bc2c450de1aea6
Ziqi He <heziqi4399@gmail.com>
2023-08-03 16:14:44 +0200

    add MensaID type

--- backend/src/scrapers/mensa.d.ts
+++ backend/src/scrapers/mensa.d.ts
@@ -1,11 +1,27 @@
 /** This file contains type of mensa information. */
 
 /** Type of Mensa object */
-interface MensaDetailedInfo {
+interface MensaInfo {
   id: string;
   name: string;
   url: string;
 }
 
 /** Type of Mensa ID */
-type MensaID = 'sued' | 'lmpl' | 'mohm' | 'isch';
+type MensaID =
+  | 'sued'
+  | 'lmpl'
+  | 'mohm'
+  | 'isch'
+  | 'cafeteria-kochstrasse'
+  | 'regb'
+  | 'spau'
+  | 'cafeteria-veilstr'
+  | 'hohf'
+  | 'cafeteria-langegasse'
+  | 'baer'
+  | 'bingstrasse'
+  | 'eich'
+  | 'ingo'
+  | 'ansb'
+  | 'trie';
