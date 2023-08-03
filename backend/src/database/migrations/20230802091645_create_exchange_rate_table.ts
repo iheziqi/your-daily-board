a41dea739c903d40aa9df1cbdb4425588982b1d8
Ziqi He <heziqi4399@gmail.com>
2023-08-03 17:51:01 +0200

    add change_from_yesterday to table exchange_rate

--- backend/src/database/migrations/20230802091645_create_exchange_rate_table.ts
+++ backend/src/database/migrations/20230802091645_create_exchange_rate_table.ts
@@ -5,6 +5,7 @@ export async function up(knex: Knex): Promise<void> {
     table.string('from_to').primary();
     table.date('date');
     table.float('exchange_rate');
+    table.float('change_from_yesterday');
   });
 }
 
