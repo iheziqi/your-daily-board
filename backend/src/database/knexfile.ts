cfb3d70ee930b047d194fa11be9ef07d80582b14
Ziqi He <heziqi4399@gmail.com>
2023-08-04 22:52:52 +0200

    fix: database connection env variables

--- backend/src/database/knexfile.ts
+++ backend/src/database/knexfile.ts
@@ -9,6 +9,7 @@ const config: {[key: string]: Knex.Config} = {
     client: 'mysql2',
     connection: {
       host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT!, 10),
       database: process.env.DB_DATABASE,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
