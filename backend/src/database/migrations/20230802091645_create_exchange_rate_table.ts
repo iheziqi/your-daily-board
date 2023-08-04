ac7014874ec08a32725ca1d6f7d1e15786f98279
Ziqi He <heziqi4399@gmail.com>
2023-08-04 14:24:37 +0200

    Improve database structure

--- backend/src/database/migrations/20230802091645_create_exchange_rate_table.ts
+++ backend/src/database/migrations/20230802091645_create_exchange_rate_table.ts
@@ -3,9 +3,9 @@ import {Knex} from 'knex';
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('exchange_rate', table => {
     table.string('from_to').primary();
-    table.date('date');
-    table.float('exchange_rate');
-    table.float('change_from_yesterday');
+    table.string('date');
+    table.float('exchange_rate', 8, 4);
+    table.float('change_from_yesterday', 8, 4);
   });
 }
 
--- backend/src/database/migrations/20230802093834_create_subscriptions_table.ts
+++ backend/src/database/migrations/20230802093834_create_subscriptions_table.ts
@@ -1,26 +1,42 @@
 import {Knex} from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable('subscriptions', table => {
-    table.integer('user_id').unsigned();
-    table
-      .foreign('user_id')
-      .references('users.id')
-      .onUpdate('CASCADE')
-      .onDelete('CASCADE');
-    table.string('menu_sub').nullable();
-    table
-      .foreign('menu_sub')
-      .references('mensa_info.id')
-      .onUpdate('CASCADE')
-      .onDelete('CASCADE');
-    table.string('exchange_rate_sub').nullable();
-    table
-      .foreign('exchange_rate_sub')
-      .references('exchange_rate.from_to')
-      .onDelete('CASCADE')
-      .onUpdate('CASCADE');
-  });
+  return knex.schema
+    .createTable('menu_subscriptions', table => {
+      table.integer('user_id').unsigned();
+      table
+        .foreign('user_id')
+        .references('users.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+
+      table.string('menu_sub');
+      table
+        .foreign('menu_sub')
+        .references('mensa_info.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+
+      table.primary(['user_id', 'menu_sub']);
+    })
+    .createTable('exchange_rate_subscriptions', table => {
+      table.integer('user_id').unsigned();
+      table
+        .foreign('user_id')
+        .references('users.id')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      table;
+
+      table.string('exchange_rate_sub');
+      table
+        .foreign('exchange_rate_sub')
+        .references('exchange_rate.from_to')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+
+      table.primary(['user_id', 'exchange_rate_sub']);
+    });
 }
 
 export async function down(knex: Knex): Promise<void> {
