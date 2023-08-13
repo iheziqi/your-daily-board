772380635a7c4dbd9bc9a27d34134af7f093cbd0
Ziqi He <heziqi4399@gmail.com>
2023-08-13 16:33:30 +0200

    refactor: use index.ts to export all repositories

---
+++ backend/src/repositories/index.ts
@@ -0,0 +1,13 @@
+import ExchangeRepository from './ExchangeRepository';
+import MensaInfoRepository from './MensaInfoRepository';
+import MensaMenuRepository from './MensaMenuRepository';
+import SubscriptionRepository from './SubscriptionRepository';
+import UserRepository from './UserRepository';
+
+export {
+  ExchangeRepository,
+  MensaInfoRepository,
+  MensaMenuRepository,
+  SubscriptionRepository,
+  UserRepository,
+};
