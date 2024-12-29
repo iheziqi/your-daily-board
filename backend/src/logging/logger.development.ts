4cf09a33c5b171365940de9d4766c6dad32e7eba
Ziqi He <heziqi4399@gmail.com>
2024-12-29 15:56:35 +0100

    feat: winston logger

---
+++ backend/src/logging/logger.development.ts
@@ -0,0 +1,19 @@
+import { createLogger, format, transports } from 'winston';
+const { combine, timestamp, json, colorize } = format;
+
+const consoleLogFormat = format.combine(
+  format.colorize(),
+  format.printf(({ level, message, timestamp }) => {
+    return `[${timestamp}]${level}: ${message}`;
+  })
+);
+
+export const logger = createLogger({
+  level: 'info',
+  format: combine(colorize(), timestamp(), json()),
+  transports: [
+    new transports.Console({
+      format: consoleLogFormat,
+    }),
+  ],
+});
